fc256796405fb900f26dc1c924782258f85b72d4
Vesa Karvonen <vesa.a.j.k@gmail.com>
2024-09-16 08:35:14 +0300

    Simplify Picos example

--- test/test_picos_io_cohttp.ml
+++ test/test_picos_io_cohttp.ml
@@ -4,19 +4,13 @@ module Actor = Actor_picos_io_cohttp
 let n_domains = 4
 let username = Sys.argv.(1)
 
-let main () =
-  Flock.join_after @@ fun () ->
-  for _ = 1 to n_domains do
-    Flock.fork @@ fun () ->
-    let client = Actor.client ~username in
-
-    while true do
-      let sub = Actor.request client in
-      let img = Actor.render sub in
-      Actor.respond client sub img;
-
-      Control.sleep ~seconds:(0.2 +. Random.float 0.03)
-    done
+let () =
+  Picos_mux_multififo.run_on ~n_domains @@ fun () ->
+  let client = Actor.client ~username in
+  Run.all @@ List.init n_domains
+  @@ fun _ () ->
+  while true do
+    let sub = Actor.request client in
+    let img = Actor.render sub in
+    Actor.respond client sub img
   done
-
-let () = Picos_mux_multififo.run_on ~n_domains main
